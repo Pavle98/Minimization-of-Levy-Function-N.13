4ac2f757a1e5d266ecd04d672a3b46503426d8e4
Pavle98 <pavlevukovic98@gmail.com>
2022-06-07 01:10:39 +0200

    Refactored and added comments

--- Levi13/src/levi13/Main.java
+++ Levi13/src/levi13/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.Math;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -21,9 +20,11 @@ public class Main {
 	static int[] scope = { -10, 10 }; //  (given interval)
 	static int tournamentPopulationNumber = 4; //numbe of chroms that participate in tournamate in selection 
 
+
 	public static void main(String[] args) {
-		myalgorithm();
+		geneticAlgorithm();
 	}
+
 	//Deffinition of LEVY FUNCTION N. 13
 	public static double fitness_function(Chromosome chroms) {
 		double x = chroms.x;
@@ -42,23 +43,22 @@ public class Main {
 		return chroms;
 	}
 
-	//
+	//selecting best chromosome from tournament of (size) random chromosomes
 	public static Chromosome selection(ArrayList<Chromosome> population, int size) {
-		ArrayList<Chromosome> tournamentList = new ArrayList<Chromosome>();
+		ArrayList<Chromosome> tournamentList = new ArrayList<>();
 		Random rand = new Random();
 		int randChrom = rand.nextInt(population.size());
 		double selectedValue = 0;
 		Chromosome bestChrom = null;
-		double bestFunctionValue = -100; /// ovo vrv nije dobro
+		double bestFunctionValue = -100;
 
-		while (tournamentList.size() < population.size()) {
+		while (tournamentList.size() < size) {
 			tournamentList.add(population.get(randChrom));
-			//System.out.println(randChrom + " rando chrom ");
 		}
 
 		for (Chromosome selectedChrom : tournamentList) {
 			selectedValue = fitness_function(selectedChrom);
-		
+
 			if (bestChrom == null || selectedValue > bestFunctionValue) {
 				bestFunctionValue = selectedValue;
 				bestChrom = selectedChrom;
@@ -68,46 +68,58 @@ public class Main {
 		return bestChrom;
 	}
 	//BLX-alpha crossover
-	public static ArrayList<Chromosome> crossover(Chromosome firstChrom, Chromosome secondChrom) {
-		ArrayList<Chromosome> newChroms = new ArrayList<Chromosome>();
-		Chromosome firstNewChrom = new Chromosome();
-		Chromosome SecondNewChrom = new Chromosome();
-		newChroms.add(firstNewChrom);
-		newChroms.add(SecondNewChrom);
+	public static ArrayList<Chromosome> crossover(Chromosome firstParent, Chromosome secondParent) {
+		ArrayList<Chromosome> newChroms = new ArrayList<>();
+		Chromosome firstChild = new Chromosome();
+		Chromosome secondChild = new Chromosome();
+		newChroms.add(firstChild);
+		newChroms.add(secondChild);
 		Random rand = new Random();
 		double d;
-		for (int i = 0; i < 2; i++) {
-			if (i == 0) {
-				d = Math.abs(firstChrom.x - secondChrom.x);
-				double min = Math.min(firstChrom.x, secondChrom.x) - (alpha * d);
-				double max = Math.max(firstChrom.x, secondChrom.x) + (alpha * d);
-				double u = rand.nextDouble() * (max - min) + min;
-				firstNewChrom.x = u;
+		double min;
+		double max;
+		double u;
+		//Setting first and second children's X  based on BLX-a crossover
+		d = Math.abs(firstParent.x - secondParent.x);
+		min= Math.min(firstParent.x, secondParent.x) - (alpha * d);
+		max = Math.max(firstParent.x, secondParent.x) + (alpha * d);
+		u = rand.nextDouble() * (max - min) + min;
+		firstChild.x = u;
 
-				min = Math.min(firstChrom.x, secondChrom.x) - (alpha * d);
-				max = Math.max(firstChrom.x, secondChrom.x) + (alpha * d);
-				u = rand.nextDouble() * (max - min) + min;
-				SecondNewChrom.x = u;
-			} else {
-				d = Math.abs(firstChrom.y - secondChrom.y);
-				double min = Math.min(firstChrom.y, secondChrom.y) - (alpha * d);
-				double max = Math.max(firstChrom.y, secondChrom.y) + (alpha * d);
-				double u = rand.nextDouble() * (max - min) + min;
-				firstNewChrom.y = u;
+		u = rand.nextDouble() * (max - min) + min;
 
-				min = Math.min(firstChrom.y, secondChrom.y) - (alpha * d);
-				max = Math.max(firstChrom.y, secondChrom.y) + (alpha * d);
-				u = rand.nextDouble() * (max - min) + min;
-				SecondNewChrom.y = u;
-			}
+		secondChild.x = u;
 
-		}
+		//Setting Y
+		d = Math.abs(firstParent.y - secondParent.y);
+		min = Math.min(firstParent.y, secondParent.y) - (alpha * d);
+		max = Math.max(firstParent.y, secondParent.y) + (alpha * d);
+		u = rand.nextDouble() * (max - min) + min;
+		firstChild.y = u;
+
+		min = Math.min(firstParent.y, secondParent.y) - (alpha * d);
+		max = Math.max(firstParent.y, secondParent.y) + (alpha * d);
+		u = rand.nextDouble() * (max - min) + min;
+		secondChild.y = u;
+
 		return newChroms;
 
 	}
+	//Just filling population with chromosomes with random values from the scope
+	public static void initialization (ArrayList<Chromosome> initialPopulation){
 
-	public static void myalgorithm() {
-		 BufferedWriter writer = null;
+		for (int popi = 0; popi < populationNumber; popi++) {
+			Chromosome andAnotherOne = new Chromosome();
+			andAnotherOne.x = scope[0] + (Math.random() * scope[1] * 2);
+			andAnotherOne.y = scope[0] + (Math.random() * scope[1] * 2);
+
+			initialPopulation.add(andAnotherOne);
+		}
+
+	}
+
+	public static void geneticAlgorithm() {
+		BufferedWriter writer = null;
 		try {
 			writer = new BufferedWriter(new FileWriter("Chroms.txt"));
 		} catch (IOException e1) {
@@ -115,37 +127,38 @@ public class Main {
 			e1.printStackTrace();
 		}
 		System.out.println("Alg starting");
-		int newPopulationSize = populationNumber;
 		Chromosome bestChromosome;
-		ArrayList<Chromosome> initialPopulation = new ArrayList<Chromosome>();
-		ArrayList<Chromosome> newPopulation = new ArrayList<Chromosome>();
+		ArrayList<Chromosome> initialPopulation;
+		ArrayList<Chromosome> newPopulation = new ArrayList<>();
 		double bestFitness = -100;
 		int iter = 0;
 		for (int i = 0; i < repetitions; i++) {
-			initialPopulation = new ArrayList<Chromosome>();
+			//ALgorithm repetitions, instead of starting program multiple times, we have
+			//this for loop in which we reset initial parameters for each repetition
+			// such as initial poppulation, best chrom and fitness, after that, we fill initialPop with random chroms
+			initialPopulation = new ArrayList<>();
 			iter = 0;
 			System.out.println("repetition " + i);
 			bestChromosome = new Chromosome();
 			bestChromosome.fitnessFunction = 1000.0;
 			bestFitness = 100;
 
-			for (int popi = 0; popi < populationNumber; popi++) {
-				Chromosome andAnotherOne = new Chromosome();
-				andAnotherOne.x = scope[0] + (Math.random() * scope[1] * 2);
-				andAnotherOne.y = scope[0] + (Math.random() * scope[1] * 2);
-				
-				initialPopulation.add(andAnotherOne);
-			}
-			
+			initialization(initialPopulation);
 
-			while (bestChromosome.fitnessFunction > 0.0000001 && iter < maxIterations && (bestChromosome.x != 1.0 && bestChromosome.y != 1.0 )) {
+			//We are going through generations until we are satisfied with fitness function (7th decimal)
+			//Or until we have done enough (specified number) of generations
+			while (bestChromosome.fitnessFunction > 0.0000001 && iter < maxIterations) {
 
 				newPopulation.removeAll(newPopulation);
-					for(int ra = 0; ra <populationNumber; ra++) {
-						newPopulation.add(initialPopulation.get(ra));
-					}
+				for(int ra = 0; ra <populationNumber; ra++) {
+					newPopulation.add(initialPopulation.get(ra));
+				}
+				//after adding 100 of old chromosomes (From previous generation)
+				//we are now adding 100 new chromosomes that have been through selection, crossover and mutation
+				//thats why we have this condition
 				while(newPopulation.size() < (populationNumber*2)) {
-				
+
+					//First we choose 2 chars via selection (Tournament)
 					Chromosome chr1 = selection(initialPopulation, tournamentPopulationNumber);
 					Chromosome chr2 = selection(initialPopulation, tournamentPopulationNumber);
 					ArrayList<Chromosome> chrsFromCrossover = crossover(chr1, chr2);
@@ -156,18 +169,21 @@ public class Main {
 					newPopulation.add(chr3);
 					newPopulation.add(chr4);
 				}
+				//We are sorting old + new chromosomes (Offsprings) by fitness value
+				//(200 in total), where we will only choose top 100 for new generation
 				for(Chromosome newPopChrom : newPopulation) {
 					newPopChrom.fitnessFunction = fitness_function(newPopChrom);
 				}
+
 				Collections.sort(newPopulation);
-				initialPopulation.removeAll(newPopulation);
+				initialPopulation.removeAll(initialPopulation);
 				for(int r = 0; r <populationNumber; r++) {
 					initialPopulation.add(newPopulation.get(r));
 				}
-				for(int r = 0; r <200; r++) {
-				}
+
+				//Calculating total, best and avg fitnesses, and writing them into file
 				double bestFitValue = fitness_function(newPopulation.get(0));
-				
+
 				if (bestFitness > bestFitValue) {
 					bestFitness = bestFitValue;
 					bestChromosome = newPopulation.get(0);
@@ -177,29 +193,29 @@ public class Main {
 					totalfitness+= newPopulation.get(ik).fitnessFunction;
 				}
 				double avg = totalfitness / newPopulation.size();
-				
-				
+
+
 				String data = "Generation" + iter+":"+ " Best chromosome fitness =  " +bestFitness  + " Average  ="  + avg + "\n";
 				System.out.printf("Generation " + iter +":"+ " Best chromosome fitness = %.10f "  + " Average  ="  + avg + "\n", bestFitness);
-			    try {
-			        writer.write(data);
-		        } catch (Exception e) {
-		            e.printStackTrace();
-		        }
-				
+				try {
+					writer.write(data);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
 				iter += 1;
 			}
-			
-			
+
+
 			System.out.println("Best chromosome [" +bestChromosome.x +"] ["+ bestChromosome.y + "]");
 			try {
-				writer.write("Best chromosome [" +bestChromosome.x +"] ["+ bestChromosome.y + "] \n");		
+				writer.write("Best chromosome [" +bestChromosome.x +"] ["+ bestChromosome.y + "] \n");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 		try {
-			
+
 			writer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
